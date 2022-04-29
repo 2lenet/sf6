f87b7ef537a1c35f37ef442afc2faa864362d0d4
sebastien <2le@2le.net>
2022-04-29 14:55:45 +0200

    add crudit elems

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const Encore = require('@symfony/webpack-encore');
-const path = require('path');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
@@ -10,17 +9,9 @@ Encore
     .setPublicPath('/build')
 
     .addEntry('app', './assets/scripts/app.js')
-    .addEntry('vue', './assets/vue/main.ts')
     .addStyleEntry('style', './assets/styles/app.scss')
 
-    .enableSassLoader((config) => {
-        config.additionalData = '@import "assets/styles/settings/index";';
-    })
-    .enableVueLoader()
-    .enableTypeScriptLoader()
-    .addAliases({
-        '@': path.resolve('./assets/vue/')
-    })
+    .enableSassLoader()
 
     .splitEntryChunks()
     .disableSingleRuntimeChunk()
