080b6bbf4bc6ca89402de73fa1a4d5904a01dee1
Lollipop <31731763+SpaghettiBolognaise@users.noreply.github.com>
2022-07-20 15:44:06 +0200

    fix: use orm not odm

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -9,4 +9,4 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
 $kernel->boot();
 
-return $kernel->getContainer()->get('doctrine_mongodb.odm.default_document_manager');
+return $kernel->getContainer()->get('doctrine')->getManager();
